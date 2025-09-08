b07ccb665187c9fce5b338aca8367bf421dd45e1
KOT22-hub <munatsi272@gmail.com>
2025-09-08 15:17:34 +0200

    commited

--- frontend/src/container/Gallery/Gallery.jsx
+++ frontend/src/container/Gallery/Gallery.jsx
@@ -55,8 +55,7 @@ const Gallery = () => {
 
         </div>
         <div className='app__gallery-images_arrows'>
-          <BsArrowLeftShort className='gallery__arrow-icon' onClick={() => scroll('left')} />
-          <BsArrowRightShort className='gallery__arrow-icon' onClick={() => scroll('right')} />
+       
 
 
         </div>
